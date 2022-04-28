55d77cd56d953f9745805463695d98658d44149a
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-04-27 21:45:56 -0700

    Added questions and answers

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-var startBtn = document.getElementById("start"); 
-var questionContainer = document.getElementById("question-container");
+var startBtn = document.querySelector("start"); 
+var questionContainer = document.querySelector("question-container");
 
 startBtn.addEventListener("click", startGame); 
 
 function startGame() {
     startBtn.setAttribute("style", "display: none;");
     questionContainer.setAttribute("style", "display: block"); 
-    nextQuestion()
+    nextQuestion();
 };
 
-function nextQuestion() {}; 
+function nextQuestion() {
+    // How do I move sequentially through my array? 
+    questions++ 
+
+}; 
+
+const questions = [
+   { 
+    question: "Commonly used data types do NOT include:", 
+    answers: [
+        {answer: "strings", correct: false}, 
+        {answer: "booleans", correct: false}, 
+        {answer: "alerts", correct: true}, 
+        {answer: "numbers", correct: false}, 
+    ]
+   }, 
+   {
+    question: "The condition within an if/else statement is enclosed within ___", 
+    answers: [
+        {answer: "quotes", correct: false},  
+        {answer: "curly brackets", correct: false},   
+        {answer: "square brackets", correct: false}, 
+        {answer: "parentheses", correct: true}, 
+    ]
+    }, 
+    {
+    question: "Arrays in Javascript can be used to store ____", 
+    answers: [
+        {answer: "numbers and strings", correct: false}, 
+        {answer: "other arrays", correct: false}, 
+        {answer: "booleans", correct: false}, 
+        {answer: "all of the above", correct: true}, 
+    ]
+    }, 
+    {
+    question: "String values must be enclosed within ____ when assigned to a variable.", 
+    answers: [
+        {answer: "square brackets", correct: false}, 
+        {answer: "quotes", correct: true}, 
+        {answer: "commas", correct: false}, 
+        {answer: "curly brackets", correct: false}, 
+    ]
+    }, 
+    {
+    question: "A useful tool during development and debugging for printing content to the debugger is:", 
+    answers: [
+        {answer: "console log", correct: true}, 
+        {answer: "Javascript", correct: false}, 
+        {answer: "terminal/bash", correct: false}, 
+        {answer: "for loops", correct: false}, 
+    ]
+    },
+] 
