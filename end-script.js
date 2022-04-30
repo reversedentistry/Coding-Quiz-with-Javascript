8bba5af50c14dd49225b7e28427dc0fb9c2c0a88
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-04-30 16:19:27 -0700

    Added function upon initialization to display final score

--- end-script.js
+++ end-script.js
@@ -1,2 +1,9 @@
 var finalScoreDisplay = document.querySelector("#score-display"); 
 var submitBtn = document.querySelector("#submit-score"); 
+
+function init() {
+    var mostRecentScore = localStorage.getItem("finalscore"); 
+    finalScoreDisplay.textContent = "You scored " + mostRecentScore + " points!"
+}
+
+init(); 
