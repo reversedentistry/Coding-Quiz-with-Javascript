19fe8a3c4b2a6b7eb31af8f16d8199659adf5470
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-04-30 22:43:01 -0700

    Added function that saves player info for high score display

--- end-script.js
+++ end-script.js
@@ -1,9 +1,11 @@
 var finalScoreDisplay = document.querySelector("#score-display"); 
 var playerName = document.querySelector("#player-name"); 
 var submitBtn = document.querySelector("#submit-score"); 
+var mostRecentScore = localStorage.getItem("finalscore");
+
+var highScores = JSON.parse(localStorage.getItem("highScores")) || []; 
 
 function init() {
-    var mostRecentScore = localStorage.getItem("finalscore"); 
     finalScoreDisplay.textContent = "You scored " + mostRecentScore + " points!"
 }
 
@@ -12,3 +14,21 @@ init();
 playerName.addEventListener("keyup", function ensureEntry() {
     submitBtn.disabled = !playerName.value
 }); 
+
+function saveScore(event) {
+    event.preventDefault(); 
+    var playerInfo = {
+        score: mostRecentScore,
+        name: playerName.value
+    }
+
+    highScores.push(playerInfo);
+    
+    highScores.sort(function(a,b) {
+        return b.score - a.score
+    }); 
+
+    highScores.splice(5); 
+
+    localStorage.setItem("highScores", JSON.stringify(highScores)); 
+}
