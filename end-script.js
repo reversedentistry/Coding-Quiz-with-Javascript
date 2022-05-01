ffb77016d9bfe374e72df8ea5b04230c7f4b9631
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-04-30 23:28:32 -0700

    Added comments

--- end-script.js
+++ end-script.js
@@ -3,6 +3,7 @@ var playerName = document.querySelector("#player-name");
 var submitBtn = document.querySelector("#submit-score"); 
 var mostRecentScore = localStorage.getItem("finalscore");
 
+// This pulls either an existing array from local storage or, if none exist, creates an empty array
 var highScores = JSON.parse(localStorage.getItem("highScores")) || []; 
 
 function init() {
@@ -11,6 +12,7 @@ function init() {
 
 init(); 
 
+// Ensures that submit button will only function if a key entry has been detected
 playerName.addEventListener("keyup", function ensureEntry() {
     submitBtn.disabled = !playerName.value
 }); 
@@ -21,13 +23,14 @@ submitBtn.addEventListener("click", function saveScore(event) {
         score: mostRecentScore,
         name: playerName.value
     }
-
+// Pushes the object into the array created above
     highScores.push(playerInfo);
-    
+// Sorts the contents of the array from highest score to lowest     
     highScores.sort(function(a,b) {
         return b.score - a.score
     }); 
-
+    console.log(highScores); 
+// Truncates the array to only 5 entries when too many entries are logged - useful for leaderboard later    
     highScores.splice(5); 
 
     localStorage.setItem("highScores", JSON.stringify(highScores)); 
